deec7d10eda9d140f813f737d6c4bf913bd4e37c
Bilal Almazbekov <138653003+bilyashik@users.noreply.github.com>
2023-07-25 18:17:57 +0600

    Add files via upload

---
+++ 2_5282742635151240615/js/modal.js
@@ -0,0 +1,84 @@
+const modal = document.querySelector('.modal');
+const modalTrigger = document.querySelector('#btn-get');
+const modalCloseButton = document.querySelector('.modal_close');
+
+const openModal = () => {
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+};
+
+const closeModal = () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
+};
+
+modalTrigger.addEventListener('click', openModal);
+modalCloseButton.addEventListener('click', closeModal);
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+let isModalShown = false;
+
+const handleScroll = () => {
+    const isAtBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight;
+
+    if (isAtBottom && !isModalShown) {
+        openModal();
+        isModalShown = true;
+    }
+};
+
+window.addEventListener('scroll', handleScroll);
+
+
+setTimeout(() => {
+    openModal();
+    isModalShown = true;
+}, 10000);
+
+
+// POST DATA
+
+// const form = document.querySelector("form")
+//
+// const postData = (form) => {
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault()
+//
+//
+//         const request = new XMLHttpRequest()
+//         request.open("POST", "server.php")
+//         request.setRequestHeader("Content-type", "application/json")
+//
+//         const formData = new FormData(form)
+//         const obj = {}
+//         formData.forEach( (item, i) => {
+//             obj[i] = item
+//         })
+//
+//
+//         request.send()
+//     })
+// }
+// postData(form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
